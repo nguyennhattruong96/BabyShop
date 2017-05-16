fc0e80f4a945e6c8a89be2f5b652487bb126b7e1
NhatTruong <nguyennhattruong7096@gmail.com>
2017-05-16 16:21:12 +0700

    Add project files.

---
+++ BabyShop/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using BabyShop.Models;
+
+namespace BabyShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        BabyShopEntities db = new BabyShopEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            List<AspNetRole> role = db.AspNetRoles.ToList<AspNetRole>();
+            role[0].AspNetUsers.Add()
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
